8205db133657e4ad883ecaaa2b713fd29b6b6742
Chittrang Mishra <chittrang.m@HCL.COM>
2018-08-07 14:21:21 +0530

    changed directionalurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   accessTokenMapbox: 'pk.eyJ1IjoiY2hpdHRyYW5nIiwiYSI6ImNqanA3dHA5NTd3MjEzcGxmdDV0Njc4NXQifQ.NapWZkvR3_lYOqXQkZg99A',//'pk.eyJ1Ijoid3lra3NzIiwiYSI6ImNqMjR6aTdmdzAwNHMzMnBvbjBucjlqNm8ifQ.6GjGpofWBVaIuSnhdXQb5w',
-  directionsURL : 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic',
+  directionsURL : 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/',
   apiurl:'https://oa3fc6wq4d.execute-api.ap-southeast-2.amazonaws.com/test',//'/api',
   apiauthkey:'12345678901234567890'
 };
